3a86e94f9c465d707f29bcbbb5857cd59bb4f8e4
Addou <Addou.ismail@gmail.com>
2016-02-05 23:58:45 +0000

    nothing

--- src/main/java/com/ehub/controllers/ProductController.java
+++ src/main/java/com/ehub/controllers/ProductController.java
@@ -1,9 +1,9 @@
 package com.ehub.controllers;
 
-import com.ehub.entities.Admin;
+
 import com.ehub.entities.Product;
 import com.ehub.services.ProductService;
-import com.ehub.services.ProductServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,29 +15,33 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-
     private ProductService productService;
 
     @Autowired
     public void setProductService(ProductService productService) {
         this.productService = productService;
     }
+
     @RequestMapping(method= RequestMethod.GET)
     public Iterable listProducts(){
         return productService.findAll();
     }
+
     @RequestMapping(method= RequestMethod.POST)
     public Product insertProduct(Product product){
         return productService.insert(product);
     }
+
     @RequestMapping(value="/{id}",method= RequestMethod.GET)
     public Product getProductById(@PathVariable String id){
         return productService.findById(id);
     }
+
     @RequestMapping(method=RequestMethod.PUT)
     public Product updateProduct(@RequestBody Product product){
         return productService.update(product);
     }
+
     @RequestMapping(value="/{id}",method=RequestMethod.DELETE)
     public Long deleteProductById(@PathVariable String id){
         return productService.deleteById(id);
